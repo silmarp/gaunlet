f381a6e274c30a39a57c86c163eff73d3e2ae5b3
Silmar <silmarjr2@gmail.com>
2023-05-15 16:09:54 -0300

    create flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  # TODO use napalm for automating builds
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          hello = pkgs.hello;
+          default = hello;
+        };
+        apps = rec {
+          hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
+          default = hello;
+        };
+        devShells = {
+          default = with pkgs; mkShell {
+            buildInputs = [
+              nodejs
+            ];
+          };
+        };
+      }
+    );
+}
