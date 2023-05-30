60628234c004cc07f5f0ea61134a143f60fd2ed4
Rafael <rafajdantas@gmail.com>
2023-05-30 15:42:31 -0300

    add appbar, footer and icmc logo

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-footer
+        color="primary"
+        >
+            <v-card
+            flat
+            color="primary"
+            >
+                <v-card-text>
+                    <strong>SSC0961 - Desenvolvimento Web e Mobile</strong>
+                </v-card-text>
+            </v-card>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
