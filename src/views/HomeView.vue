d29a77da16dc2615a896468982e97b77fcc0b436
Rafael <rafajdantas@gmail.com>
2023-06-01 16:29:54 -0300

    tentativa inicial de ler a api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,50 @@
 <template>
-  <hello-world />
+  <div>
+    <div>
+      <v-btn @click="loadDefesas()">
+        teste
+      </v-btn>
+    </div>
+    <v-table
+    fixed-header
+    height="500px">
+      <thead>
+        <tr>
+          <th>
+            teste1
+          </th>
+          <th>
+            teste2
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="a in alunos" :key="a.Ordem">
+          <td>{{ a.Nome }}</td>
+          <td>{{ a.Data }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
-
 export default {
   name: 'HomeView',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      alunos: undefined,
+    };
+  },
+  methods: {
+    loadDefesas() {
+      const url = 'http://thanos.icmc.usp.br:4567/api/v1/defesas';
+      fetch(url)
+        .then((data) => (data.json()))
+        .then((response) => {
+          this.alunos = response;
+        });
+    },
   },
 };
 </script>
