7691f2b7a46760d625da8db78abc5af4e8308052
Rafael <rafajdantas@gmail.com>
2023-06-03 16:31:37 -0300

    adiciona icone e link para o github no footer

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -11,6 +11,8 @@
                     <strong>SSC0961 - Desenvolvimento Web e Mobile</strong>
                 </v-card-text>
             </v-card>
+            <v-spacer/>
+            <a href="https://github.com/silmarp/gaunlet"><v-icon large>mdi-github</v-icon></a>
         </v-footer>
     </div>
 </template>
