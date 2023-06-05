79e607ee39fc3b4e7ced79a297255a31bbff0ee3
Silmar <silmarjr2@gmail.com>
2023-06-05 00:06:07 -0300

    made about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      <div>
+        <h1>O que é Gaunlet</h1>
+        <!-- eslint-disable-next-line max-len -->
+        <p>Gaunlet é um projeto de codigo aberto feito para a diciplina SSC0961 desenvolvimento web e mobile.</p>
+        <!-- eslint-disable-next-line max-len -->
+        <p>O projeto consiste em um app que irá consumir uma API de defesas realizadas no ICMC, e mostra-las ao usuario.</p>
+      </div>
+  <div class="made by">
+    <h1>Projeto feito por:</h1>
+    <!-- TODO insert full name and Nusp of each member -->
+    <ul>
+        <li>Heitor Pupim - {Nusp}</li>
+        <li>Rafael Jun - {Nusp}</li>
+        <li>Silmar Pereira da Silva Junior - {Nusp}</li>
+    </ul>
   </div>
+  </div>
+
 </template>
