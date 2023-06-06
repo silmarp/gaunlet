942e25cef9a9bd97f4095eac2872f582a688e2f4
Silmar <silmarjr2@gmail.com>
2023-06-05 23:25:57 -0300

    refactored dialog

--- src/components/DefenceDialog.vue
+++ src/components/DefenceDialog.vue
@@ -18,24 +18,53 @@
 
       <v-card>
         <v-card-title class="text-h5 grey lighten-2">
-          Defesa
+        {{aluno}}
         </v-card-title>
 
         <v-divider></v-divider>
-        <v-card-subtitle>
-        Realizada por:{{aluno}}
+
+        <v-card-subtitle v-if="programa=='MAT'">
+          Matemática
+          <v-icon>mdi mdi-math-integral</v-icon>
         </v-card-subtitle>
 
-        <v-card-subtitle>
-        Curso:{{curso}}
+        <v-card-subtitle v-else-if="programa=='CCMC'">
+          Ciência de Computação e Matemática Computacional
+          <v-icon>mdi mdi-desktop-classic</v-icon>
         </v-card-subtitle>
 
-        <v-card-subtitle>
-        Programa:{{programa}}
+        <v-card-subtitle v-else-if="programa=='PROFMAT'">
+          Mestrado Profissional em Matemática em Rede Nacional
+          <v-icon>mdi mdi-matrix</v-icon>
+        </v-card-subtitle>
+
+        <v-card-subtitle v-else-if="programa=='PIPGEs'">
+          Programa Interinstitucional de Pós-Graduação em Estatística UFSCar-USP
+          <v-icon>mdi mdi-percent</v-icon>
+        </v-card-subtitle>
+
+        <v-card-title v-else>
+          {{programa}}
+        </v-card-title>
+
+        <v-card-subtitle v-if="curso=='ME'">
+        Mestrado
+        </v-card-subtitle>
+
+        <v-card-subtitle v-else-if="curso=='DO'">
+        Doutorado
+        </v-card-subtitle>
+
+        <v-card-subtitle v-else-if="curso=='DD'">
+        Doutorado Direto
+        </v-card-subtitle>
+
+        <v-card-subtitle v-else>
+        {{curso}}
         </v-card-subtitle>
 
         <v-card-subtitle v-if="orientador">
-        Orientador:{{orientador}}
+        Orientado por:{{orientador}}
         </v-card-subtitle>
 
         <v-divider></v-divider>
