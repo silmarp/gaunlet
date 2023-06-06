cc7b8ec9c435efbef3a0f93e42e54f16fdde9a46
Silmar <silmarjr2@gmail.com>
2023-06-05 21:14:13 -0300

    add more text to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,9 @@
         <!-- eslint-disable-next-line max-len -->
         <p>Gaunlet é um projeto de codigo aberto feito para a diciplina SSC0961 desenvolvimento web e mobile.</p>
         <!-- eslint-disable-next-line max-len -->
-        <p >O projeto consiste em um app que irá consumir uma API de defesas realizadas no ICMC, e mostra-las ao usuario.</p>
+        <p >O projeto consiste em um app que irá consumir uma API de defesas realizadas no ICMC, e apresenta-las em formato de tabela.</p>
+        <!-- eslint-disable-next-line max-len -->
+        <p>Além da apresentação das informações obtidas por meio da API é possivel também ordenar por qualquer coluna, exceto a de visualizadores e filtrar por curso, programa e nome</p>
       </div>
   <div class="made by">
 
@@ -33,9 +35,9 @@
             <a href="https://github.com/RafaelJunDantas"><v-icon large :color="hover ? 'blue': 'gray'">mdi-github</v-icon></a>
           </v-list-item-icon>
         </v-hover>
-          Rafael Jun Teramae
+          Rafael Jun Teramae Dantas
         </v-list-item-title>
-        <v-list-item-subtitle>{Nusp}</v-list-item-subtitle>
+        <v-list-item-subtitle>12563686</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
 
@@ -49,7 +51,7 @@
         </v-hover>
           Silmar Pereira da Silva Junior
         </v-list-item-title>
-        <v-list-item-subtitle>{Nusp}</v-list-item-subtitle>
+        <v-list-item-subtitle>12623950</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </div>
